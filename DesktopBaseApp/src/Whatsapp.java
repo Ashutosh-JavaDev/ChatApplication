5776cbd4654e6dddce40025b0265ecb1ac7f7221
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-08 22:33:56 +0530

    Taiilwind Project

---
+++ DesktopBaseApp/src/Whatsapp.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class Whatsapp extends JFrame {
+    public Whatsapp(){
+        setLayout(null);
+        setSize(400,600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
