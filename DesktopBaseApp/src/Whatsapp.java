c0642a8fc6388c32a6a5f3efe63dad354067b8ab
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-08 22:49:55 +0530

    Chat Application

--- DesktopBaseApp/src/Whatsapp.java
+++ DesktopBaseApp/src/Whatsapp.java
@@ -1,21 +1,36 @@
 import java.awt.Color;
 
 import javax.swing.*;
+
 public class Whatsapp extends JFrame {
-    public Whatsapp(){
-        JPanel green=new JPanel();
-        green.setBackground(Color.GREEN);
-        green.setBounds(0,0,400,50);
+    JButton send;
+    JTextField text;
+
+    public Whatsapp() {
+        JPanel green = new JPanel();
+        green.setBackground(new Color(3, 94, 3));
+        green.setBounds(0, 0, 400, 60);
         add(green);
+        // TextField
+        text = new JTextField();
+        text.setBounds(10,560,270,30);
+        add(text);
+        // Button
+        send=new JButton("Send");
+        send.setBounds(285,560,100,30);
+        send.setBackground(new Color(3,94,3));
+        send.setForeground(Color.white);
+        add(send);
         // Default setup
         setTitle("Whatsapp");
         setLayout(null);
-        setSize(400,600);
+        setSize(400, 600);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLocation(35,50);
+        setLocation(35, 50);
         setVisible(true);
     }
-    public static void main(String[]args){
+
+    public static void main(String[] args) {
         new Whatsapp();
     }
 }
