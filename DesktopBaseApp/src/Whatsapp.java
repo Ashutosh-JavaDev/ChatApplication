2ac2a4ff0e4860e2dbf7bb01c3a044f6a827610f
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-09 19:45:48 +0530

    ChatApplication

--- DesktopBaseApp/src/Whatsapp.java
+++ DesktopBaseApp/src/Whatsapp.java
@@ -1,9 +1,13 @@
 import java.awt.Color;
 import java.awt.Image;
+import java.awt.event.MouseListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
 
 import javax.swing.*;
 
-public class Whatsapp extends JFrame {
+public class Whatsapp extends JFrame implements ActionListener {
     JButton send;
     JTextField text;
 
@@ -20,7 +24,9 @@ public class Whatsapp extends JFrame {
         JLabel image=new JLabel(i2);
         image.setBounds(5,20,25,25);
         green.add(image);
-        
+        image.addMouseListener(new MouseAdapter() {
+            
+        });
         // TextField
         text = new JTextField();
         text.setBounds(10,560,270,30);
@@ -38,6 +44,10 @@ public class Whatsapp extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocation(35, 50);
         setVisible(true);
+    }
+    @Override
+    public void actionPerformed(ActionEvent ae){
+
     }
 
     public static void main(String[] args) {
