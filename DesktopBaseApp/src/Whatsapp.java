8b83c21629a5ee82dd6f00cd2ad23bdfce724610
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-10 19:54:22 +0530

    Chat Application

--- DesktopBaseApp/src/Whatsapp.java
+++ DesktopBaseApp/src/Whatsapp.java
@@ -15,29 +15,36 @@ public class Whatsapp extends JFrame implements ActionListener {
     public Whatsapp() {
         JPanel green = new JPanel();
         green.setBackground(new Color(3, 94, 3));
-        green.setBounds(0,0, 400, 60);
+        green.setBounds(0, 0, 400, 60);
         green.setLayout(null);
         add(green);
         // Image
-        ImageIcon back=new ImageIcon(ClassLoader.getSystemResource("icons/three.png"));
-        Image i1=back.getImage().getScaledInstance(25,25, Image.SCALE_DEFAULT);
-        ImageIcon i2=new ImageIcon(i1);
-        JLabel image=new JLabel(i2);
-        image.setBounds(5,20,25,25);
+        ImageIcon back = new ImageIcon(ClassLoader.getSystemResource("icons/three.png"));
+        Image i1 = back.getImage().getScaledInstance(25, 25, Image.SCALE_DEFAULT);
+        ImageIcon i2 = new ImageIcon(i1);
+        JLabel image = new JLabel(i2);
+        image.setBounds(5, 20, 25, 25);
         green.add(image);
         image.addMouseListener(new MouseAdapter() {
-            public void mouseClicked(MouseEvent ae){
+            public void mouseClicked(MouseEvent ae) {
                 System.exit(0);
             }
         });
+        // Video logo
+        ImageIcon back = new ImageIcon(ClassLoader.getSystemResource("icons/three.png"));
+        Image i1 = back.getImage().getScaledInstance(25, 25, Image.SCALE_DEFAULT);
+        ImageIcon i2 = new ImageIcon(i1);
+        JLabel image = new JLabel(i2);
+        image.setBounds(5, 20, 25, 25);
+        green.add(image);
         // TextField
         text = new JTextField();
-        text.setBounds(10,560,270,30);
+        text.setBounds(10, 560, 270, 30);
         add(text);
         // Button
-        send=new JButton("Send");
-        send.setBounds(285,560,100,30);
-        send.setBackground(new Color(3,94,3));
+        send = new JButton("Send");
+        send.setBounds(285, 560, 100, 30);
+        send.setBackground(new Color(3, 94, 3));
         send.setForeground(Color.white);
         add(send);
         // Default setup
@@ -49,8 +56,9 @@ public class Whatsapp extends JFrame implements ActionListener {
         setLocation(35, 50);
         setVisible(true);
     }
+
     @Override
-    public void actionPerformed(ActionEvent ae){
+    public void actionPerformed(ActionEvent ae) {
 
     }
 
