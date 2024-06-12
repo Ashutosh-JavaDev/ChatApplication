593d616d52df0b5ae71431c8dd1231e2fe2a3bbb
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-12 21:28:54 +0530

    Chat Application

--- DesktopBaseApp/src/User1.java
+++ DesktopBaseApp/src/User1.java
@@ -11,6 +11,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
 import java.io.IOException;
 
 import javax.swing.*;
@@ -145,6 +148,8 @@ public class User1 extends JFrame implements ActionListener {
             ServerSocket sk=new ServerSocket(6001);
             while(true){
                 Socket soc=sk.accept();
+                DataInputStream din=new DataInputStream(soc.getInputStream());
+                DataOutputStream dout=new DataOutputStream(soc.getOutputStream());
             }
         }
         catch(IOException e){
